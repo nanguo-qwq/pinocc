1d1d3baa69c396b3de191124db94ffc77f676ab0
nanguo <1721070017@qq.com>
2024-12-12 17:45:07 +0800

    original update

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,15 @@ use Pinocchio::{
     }
 };
 use std::collections::HashMap;
-use num_bigint::BigUint;
-use num_traits::Num;
 use Pinocchio::building_block::curves::bls12_381::g1_point::G1Point;
 use Pinocchio::zk::qap::equation_parser::EquationParser;
-use Pinocchio::zk::qap::gate::Gate;
 use Pinocchio::zk::qap::term::Term::{Out, TmpVar};
 
 fn main() {
-    println!("Initializing PrimeField and CRS...");
-    /*let p_hex = "1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab";
-    let p = BigUint::from_str_radix(p_hex, 16).unwrap(); // 使用BLS12-381的素数p
-    let f = PrimeField::new(&p);
-    */
+
     let f = &G1Point::curve_group();
-    let expr = "(x * x * x) + x + 5 == 35";
-    /*
-    let input = "3";
-    let expr2 = expr.replace("x",input);
-    println!("{}", expr2);
-    let eq = EquationParser::parse(&f, expr).unwrap();
-    let gates = &Gate::build(&f, &eq);
-    */
+    let expr = "x * x * x + x + 10== 40";
+
     let eq = EquationParser::parse(&f, expr).unwrap();
     let witness_map = {
         HashMap::<Term, PrimeFieldElem>::from([
@@ -50,8 +37,8 @@ fn main() {
             (Term::var("x"), f.elem(&3u8)),
             (TmpVar(1), f.elem(&9u8)),
             (TmpVar(2), f.elem(&27u8)),
-            (TmpVar(3), f.elem(&8u8)),
-            (TmpVar(4), f.elem(&35u8)),
+            (TmpVar(3), f.elem(&13u8)),
+            (TmpVar(4), f.elem(&40u8)),
             (Out, eq.rhs),
         ])
     };
@@ -66,11 +53,9 @@ fn main() {
     let verifier = Verifier::new();
     let witness_io = p.witness.io();
     let is_valid = verifier.verify(&proof, &crs, &witness_io);
+
+    println!("\nProof is valid: {}\n", is_valid);
+
     assert!(is_valid, "The proof is invalid!");
 
-    println!("Extending witness...");
-    let extended_witness = Witness::new(&witness_io, &p.witness.mid_beg);
-    println!("Witness has been extended.");
-
-    println!("All tests passed!");
 }
